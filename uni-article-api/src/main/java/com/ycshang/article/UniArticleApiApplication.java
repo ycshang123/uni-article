20c64bfd60b13f9a18769a49e20c743690fc46ed
ycshang <351234359@qq.com>
2022-03-22 17:12:19 +0800

    后端项目初始化

---
+++ uni-article-api/src/main/java/com/ycshang/article/UniArticleApiApplication.java
@@ -0,0 +1,13 @@
+package com.ycshang.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniArticleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniArticleApiApplication.class, args);
+    }
+
+}
